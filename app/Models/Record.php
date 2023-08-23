e5d4e3ca014e4787fea4cd528e4cb02c285c0dcd
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-08-23 22:54:04 +0900

    add dish tables

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['user_id', 'store_name', 'total_calory'];
+
+    public function dishes() {
+        return $this->hasMany('App\Dishes');
+    }
 }
